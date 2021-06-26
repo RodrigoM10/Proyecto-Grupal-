2f567f37b575505dbb74fa8cd779629bfb41d82f
Rodrigo <rodrigomendozar10@gmail.com>
2021-06-26 20:55:18 -0300

    Casi terminado falta modal

---
+++ index.js
@@ -0,0 +1,51 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".scrollimg0", {
+    duration:2000,
+    origin: "left",
+    distance:"-100px"
+
+});
+
+sr.reveal(".scrollimg1", {
+    duration:2000,
+    origin: "left",
+    distance:"-100px"
+
+});
+sr.reveal(".scrollimg2", {
+    duration:2000,
+    origin: "rigth",
+    distance:"-100px"
+
+});
+sr.reveal(".titulo-general", {
+    duration:2000,
+    origin: "bottom",
+    distance:"-100px"
+
+});
+
+sr.reveal(".modal1", {
+    duration:2000,
+    origin: "left",
+    distance:"-100px"
+
+});
+
+sr.reveal(".footer-10", {
+    duration:2000,
+    origin: "top",
+    distance:"-100px"
+
+});
+
+
+//Java Promociones
+
+
+
+
+
+
+
