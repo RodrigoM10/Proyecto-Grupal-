8b9ce1e516c3a01518cf1fd1e24cc465b812b8be
Rodrigo <rodrigomendozar10@gmail.com>
2021-06-29 18:37:11 -0300

    Terminado

--- index1.js
+++ index1.js
@@ -1,3 +1,49 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".scrollimg0", {
+    duration:2000,
+    origin: "left",
+    distance:"-100px"
+
+});
+
+sr.reveal(".scrollimg1", {
+    duration:2000,
+    origin: "left",
+    distance:"-100px"
+
+});
+sr.reveal(".scrollimg2", {
+    duration:2000,
+    origin: "rigth",
+    distance:"-100px"
+
+});
+sr.reveal(".titulo-general", {
+    duration:2000,
+    origin: "bottom",
+    distance:"-100px"
+
+});
+
+sr.reveal(".modal1", {
+    duration:2000,
+    origin: "left",
+    distance:"-100px"
+
+});
+
+sr.reveal(".footer-10", {
+    duration:2000,
+    origin: "top",
+    distance:"-100px"
+
+});
+
+
+
+
+
 // contador
 var tiempo = {};
 var clock = new Date("2021-06-28 5:10:00 AM"); // Obtener la fecha y almacenar en clock
